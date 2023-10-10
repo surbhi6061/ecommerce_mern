dc8348f06b5fbad4adcb42eda60dc5c4bc8eeaf8
surbhi6061 <surbhikumari6061@gmail.com>
2023-10-10 15:14:06 +0530

    package file modified

--- config/db.js
+++ config/db.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 import colors from "colors";
-
+const MONGO_URL =
+  "mongodb+srv://surbhikumari6061:YC8WrfxL2PnhwjZk@cluster0.fga4ome.mongodb.net/ecommerce";
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log(`connected to MongoDB Database ${conn.connection.host}`.bgMagenta.white);
+    const conn = await mongoose.connect(MONGO_URL);
+    console.log(
+      `connected to MongoDB Database ${conn.connection.host}`.bgMagenta.white
+    );
   } catch (error) {
     console.log(`Error in MongoDB ${error}`.bgRed.white);
   }
