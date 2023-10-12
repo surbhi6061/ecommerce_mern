fa16597a502e371799977b5f1b9e763d3604b247
surbhi kumari <106273626+surbhi6061@users.noreply.github.com>
2023-10-12 15:50:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ const app= express();
 //middlewares
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(cors(
+app.use(cors({
     "origin":"https://ecommerce-mern-frontend-coral.vercel.app"
-))
+}))
 
 //routes
 app.use("/api/v1/auth", authRoute);
